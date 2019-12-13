95bb92a9a85767a6263700fccc04abc04202efae
Gerwin Sturm <scarygami@gmail.com>
2019-12-13 23:20:56 +0100

    13: asciimatics for nice and faster visualisation

--- 13/13.py
+++ 13/13.py
@@ -1,77 +1,100 @@
 import os
 import sys
+from time import sleep
+
 currentdir = os.path.dirname(os.path.abspath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from lib.intcode import IntcodeVM
+try:
+    from lib.intcode import IntcodeVM
+except ImportError:
+    print("Intcode library could not be found")
+    exit(1)
 
-SYMBOLS = [" ", "#", "X", "_", "o"]
-
-
-def clear():
-    if os.name == "nt":
-        os.system("cls")
-    else:
-        os.system("clear")
-
-
-def print_grid(grid):
-    minx, miny = min(grid.keys())
-    maxx, maxy = max(grid.keys())
-
-    clear()
-    for y in range(miny, maxy + 1):
-        line = ""
-        for x in range(minx, maxx + 1):
-            line = line + SYMBOLS[grid[(x, y)]]
-        print(line)
+try:
+    from asciimatics.screen import Screen
+except ImportError:
+    print("Couldn't find asciimatics package, please install with `pip install asciimatics`")
+    exit(1)
 
 
-def play_game(inputfile):
+EMPTY = 0
+WALL = 1
+BLOCK = 2
+PADDLE = 3
+BALL = 4
+SYMBOLS = ["   ", "███", "███", "███", " ⬤ "]
+COLORS = [0, 7, 3, 4, 2]
+
+
+def play_game(inputfile, screen):
     source = IntcodeVM.read_intcode(inputfile)
+
+    # Insert coin...
     source[0] = 2
+
+    # Boot up arcade machine
     machine = IntcodeVM(source, silent=True)
-    outputs = machine.run()
     grid = {}
     score = 0
     total_bricks = 0
+    max_y = 0
+    move = None
+
+    # Game loop
     while machine.waiting:
+        if move is None:
+            outputs = machine.run()
+        else:
+            outputs = machine.resume([move])
+
         for o in range(0, len(outputs), 3):
             x, y, tile = outputs[o:o+3]
             if x == -1 and y == 0:
                 score = tile
             else:
+                screen.print_at(SYMBOLS[tile], x * 3, y, COLORS[tile])
                 grid[(x, y)] = tile
 
         if total_bricks == 0:
-            total_bricks = sum(1 for pos in grid if grid[pos] == 2)
+            total_bricks = sum(1 for pos in grid if grid[pos] == BLOCK)
 
-        print_grid(grid)
-        print("Bricks left: %s / %s" % (sum(1 for pos in grid if grid[pos] == 2), total_bricks))
-        print("Score: %s" % score)
+        if max_y == 0:
+            max_y = max(grid.keys())[1]
+
+        screen.print_at(
+            "Blocks left: %s / %s     " % (sum(1 for pos in grid if grid[pos] == 2), total_bricks),
+            5, max_y + 1
+        )
+        screen.print_at(
+            "Score: %s" % score,
+            5, max_y + 2
+        )
+        screen.move(0, 0)
+        screen.refresh()
+        sleep(0.01)
 
         # Breakout AI!!
-        paddle = [key for key in grid if grid[key] == 3][0]
-        ball = [key for key in grid if grid[key] == 4][0]
+        paddle = [key for key in grid if grid[key] == PADDLE][0]
+        ball = [key for key in grid if grid[key] == BALL][0]
         if paddle[0] < ball[0]:
-            value = 1
+            move = 1
         elif paddle[0] > ball[0]:
-            value = -1
+            move = -1
         else:
-            value = 0
+            move = 0
 
-        outputs = machine.resume([value])
-
-    for o in range(0, len(outputs), 3):
-        x, y, tile = outputs[o:o+3]
-        if x == -1 and y == 0:
-            score = tile
-        else:
-            grid[(x, y)] = tile
-
-    print_grid(grid)
-    print("Score: %s" % score)
+    screen.print_at(
+        "All done, press ENTER to exit!",
+        5, max_y // 2
+    )
+    screen.refresh()
+    input()
 
 
-play_game(os.path.join(currentdir, "input.txt"))
+def game(screen):
+    play_game(os.path.join(currentdir, "input.txt"), screen)
+
+
+Screen.wrapper(game)
--- 13/README.md
+++ 13/README.md
@@ -1,1 +1,5 @@
 # [Day 13: Care Package](https://adventofcode.com/2019/day/13)
+
+Uses [Asiimatics](https://pypi.org/project/asciimatics/) for output.
+
+Install with `pip install asciimatics`
