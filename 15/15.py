a7b2c7312b6e28d6a2201e4abf67e17371f9a11c
Gerwin Sturm <scarygami@gmail.com>
2019-12-15 17:18:46 +0100

    BFS > Backtracking for droid, add visualistation

--- 15/15.py
+++ 15/15.py
@@ -12,70 +12,132 @@ except ImportError:
     print("Intcode library could not be found")
     exit(1)
 
+try:
+    from asciimatics.screen import Screen
+except ImportError:
+    print("Couldn't find asciimatics package, please install with `pip install asciimatics`")
+    exit(1)
+
 WALL = 0
 FREE = 1
 OS = 2
+DROID = 3
 
-DIRECTIONS = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+COLORS = [7, 0, 4, 3]
 
-# Initiliase droid
-machine = IntcodeVM(IntcodeVM.read_intcode(os.path.join(currentdir, "input.txt")), False, True)
-machine.run()
+NORTH = 1
+SOUTH = 2
+WEST = 3
+EAST = 4
 
-os_pos = None
+MOVES = [NORTH, SOUTH, WEST, EAST]
 
-# Breadth-first search
-frontiers = [
-    # machine / position / steps
-    (machine, (0, 0), 0)
-]
-distances = {
-    (0, 0): 0
+DIRECTIONS = {
+    NORTH: (0, -1),
+    SOUTH: (0, 1),
+    WEST: (-1, 0),
+    EAST: (1, 0)
 }
-areamap = {
-    (0, 0): FREE
-}
-while len(frontiers) > 0:
-    machine, position, steps = frontiers.pop(0)
-    for m in range(len(DIRECTIONS)):
-        new_machine = machine.copy()
-        outputs = new_machine.resume([m + 1])
-        dx, dy = DIRECTIONS[m]
-        new_position = (position[0] + dx, position[1] + dy)
-        new_steps = steps + 1
-        areamap[new_position] = outputs[0]
 
-        if outputs[0] == WALL:
+REVERSE = {
+    NORTH: SOUTH,
+    SOUTH: NORTH,
+    WEST: EAST,
+    EAST: WEST
+}
+
+
+def draw_map(area_map, droid_position, screen, dimensions):
+    if not screen:
+        return
+
+    minx, miny, maxx, maxy = dimensions
+    for x in range(minx, maxx + 1):
+        for y in range(miny, maxy + 1):
+            screen.print_at("██", (x - minx) * 2, y - miny, COLORS[area_map.get((x, y), WALL)])
+
+    if droid_position:
+        x, y = droid_position
+        screen.print_at("██", (x - minx) * 2, y - miny, COLORS[DROID])
+
+    screen.refresh()
+
+
+def explore(droid, position=(0, 0), area_map=None, distance_map=None, steps=0, screen=None, dimensions=None):
+    """It felt unfair to use BFS for the first part,
+    since it explicitely stated that we have only one droid,
+    so here is one droid exploring the whole area on its own."""
+
+    if area_map is None:
+        area_map = {(0, 0): FREE}
+    if distance_map is None:
+        distance_map = {(0, 0): 0}
+
+    x, y = position
+    steps = steps + 1
+    draw_map(area_map, position, screen, dimensions)
+
+    for move in MOVES:
+        dx, dy = DIRECTIONS[move]
+        new_position = (x + dx, y + dy)
+
+        if new_position in distance_map and distance_map[new_position] <= steps:
             continue
 
-        if new_position in distances and new_steps >= distances[new_position]:
-            # this position was already reached in fewer steps before
+        distance_map[new_position] = steps
+        output = droid.resume([move])[0]
+        area_map[new_position] = output
+        if output == 0:
             continue
 
-        distances[new_position] = new_steps
+        # Continue along the new path
+        explore(droid, new_position, area_map, distance_map, steps, screen, dimensions)
 
-        if outputs[0] == OS:
-            os_pos = new_position
+        # backtracking
+        droid.resume([REVERSE[move]])
+        draw_map(area_map, position, screen, dimensions)
 
-        frontiers.append((new_machine, new_position, new_steps))
+    return (area_map, distance_map)
 
 
-print("Part 1: %s" % distances[os_pos])
+def maze(screen):
+    # Initiliase droid
+    droid = IntcodeVM(IntcodeVM.read_intcode(os.path.join(currentdir, "input.txt")), False, True)
 
-# Slightly different breadth-first search
-frontiers = [os_pos]
-minutes = 0
-while len(frontiers) > 0:
-    new_frontiers = []
-    for frontier in frontiers:
-        for (dx, dy) in DIRECTIONS:
-            new_frontier = (frontier[0] + dx, frontier[1] + dy)
-            if areamap.get(new_frontier, WALL) == FREE:
-                areamap[new_frontier] = OS
-                new_frontiers.append(new_frontier)
+    # One run to determine the dimensions of the grid
+    droid.run()
+    area_map, _ = explore(droid)
+    minx = min(pos[0] for pos in area_map)
+    maxx = max(pos[0] for pos in area_map)
+    miny = min(pos[1] for pos in area_map)
+    maxy = max(pos[1] for pos in area_map)
+    dimensions = (minx, miny, maxx, maxy)
 
-    frontiers = new_frontiers
-    if len(frontiers) > 0:
-        minutes = minutes + 1
+    # Actual run with visualization
+    droid.run()
+    area_map, distance_map = explore(droid, screen=screen, dimensions=dimensions)
+    os_pos = [pos for pos in area_map if area_map[pos] == OS][0]
 
-print("Part 2: %s" % minutes)
+    # breadth-first algorithm for spreading the oxygen
+    frontiers = [os_pos]
+    minutes = 0
+    while len(frontiers) > 0:
+        new_frontiers = []
+        for frontier in frontiers:
+            for (dx, dy) in DIRECTIONS.values():
+                new_frontier = (frontier[0] + dx, frontier[1] + dy)
+                if area_map.get(new_frontier, WALL) == FREE:
+                    area_map[new_frontier] = OS
+                    draw_map(area_map, None, screen, dimensions)
+                    new_frontiers.append(new_frontier)
+
+        frontiers = new_frontiers
+        if len(frontiers) > 0:
+            minutes = minutes + 1
+
+    input()
+    print("Part 1: %s" % distance_map[os_pos])
+    print("Part 2: %s" % minutes)
+
+
+Screen.wrapper(maze)
--- 15/README.md
+++ 15/README.md
@@ -1,2 +1,5 @@
 # [Day 15: Oxygen System](https://adventofcode.com/2019/day/15)
 
+Uses [Asiimatics](https://pypi.org/project/asciimatics/) for output.
+
+Install with `pip install asciimatics`
