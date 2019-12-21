c8a4ef52221fe8d2b15f594edcf5795b8cafd58b
Gerwin Sturm <scarygami@gmail.com>
2019-12-21 12:33:00 +0100

    21: Shorter springscript

--- 21/21.py
+++ 21/21.py
@@ -24,11 +24,10 @@ source = IntcodeVM.read_intcode(os.path.join(currentdir, "input.txt"))
 machine = IntcodeVM(source, silent=True)
 
 code1 = [
-    "NOT A J",
-    "NOT B T",
-    "OR T J",
-    "NOT C T",
-    "OR T J",
+    "OR A J",
+    "AND B J",
+    "AND C J",
+    "NOT J J",
     "AND D J",
     "WALK",
     ""
@@ -37,14 +36,12 @@ code1 = [
 print("Part 1: %s" % run_sprintcode(machine, code1))
 
 code2 = [
-    "NOT A J",
-    "NOT B T",
-    "OR T J",
-    "NOT C T",
-    "OR T J",
+    "OR A J",
+    "AND B J",
+    "AND C J",
+    "NOT J J",
     "AND D J",
-    "NOT H T",
-    "NOT T T",
+    "OR H T",
     "OR E T",
     "AND T J",
     "RUN",
