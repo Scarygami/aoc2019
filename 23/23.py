a12fd572642c8ffc46fdcf0528c580c7a468c80b
Gerwin Sturm <scarygami@gmail.com>
2019-12-23 12:10:26 +0100

    23: Some cleanup

--- 23/23.py
+++ 23/23.py
@@ -21,7 +21,10 @@ def handle_outputs(packets, outputs, n):
         dest = outputs[n][o]
         x = outputs[n][o + 1]
         y = outputs[n][o + 2]
-        packets[dest].append([x, y])
+        if dest == 255:
+            packets[dest] = [[x, y]]
+        else:
+            packets[dest].append([x, y])
 
     outputs[n] = outputs[n][end:]
 
@@ -38,8 +41,10 @@ def initialise_network(source):
         idle[n] = 0
         packets[n] = []
         outputs[n] = []
-        outputs[n].extend(machine.run([n]))
-        handle_outputs(packets, outputs, n)
+        output = machine.run([n])
+        if len(output) > 0:
+            outputs[n].extend(machine.run([n]))
+            handle_outputs(packets, outputs, n)
 
         machine.stepwise = True
         machines[n] = machine
@@ -53,9 +58,9 @@ def run_network(source, nat=False):
 
     while True:
         if nat:
-            if len([1 for state in idle if idle[state] > 500]) == 50:
+            if len([1 for state in idle if idle[state] > 10]) == 50 and len(packets[255]) > 0:
                 # all machines have been idle for a while
-                nat_value = packets[255][-1]
+                nat_value = packets[255][0]
                 packets[0].append(nat_value)
                 idle[0] = 0
                 if last_nat_value and last_nat_value == nat_value[1]:
@@ -69,6 +74,7 @@ def run_network(source, nat=False):
                 if len(packets[n]) > 0:
                     packet = packets[n].pop(0)
                     output = machine.resume(packet)
+                    idle[n] = 0
                 else:
                     output = machine.resume([-1])
                     idle[n] = idle[n] + 1
