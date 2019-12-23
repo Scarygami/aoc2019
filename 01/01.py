9190a46b6b98112f251edaec13e24ed2dab7bde1
Gerwin Sturm <scarygami@gmail.com>
2019-12-23 16:54:41 +0100

    01: Cleanup

--- 01/01.py
+++ 01/01.py
@@ -1,15 +1,12 @@
 import os
 currentdir = os.path.dirname(os.path.abspath(__file__))
 
+
 def calculate_fuel(mass):
     """Calculates the fuel necessary for the provided mass."""
 
     return (mass // 3) - 2
 
-assert calculate_fuel(12) == 2
-assert calculate_fuel(14) == 2
-assert calculate_fuel(1969) == 654
-assert calculate_fuel(100756) == 33583
 
 def calculate_total_fuel(mass):
     """Calculates the fuel necessary for the provided mass including fuel."""
@@ -19,9 +16,6 @@ def calculate_total_fuel(mass):
         return 0
     return fuel + calculate_total_fuel(fuel)
 
-assert calculate_total_fuel(14) == 2
-assert calculate_total_fuel(1969) == 966
-assert calculate_total_fuel(100756) == 50346
 
 def total_fuel(inputfile, include_fuel=True):
     """Calculates the total fuel for the provided puzzle input file.
@@ -45,8 +39,16 @@ def total_fuel(inputfile, include_fuel=True):
 
     return fuel
 
+
+assert calculate_fuel(12) == 2
+assert calculate_fuel(14) == 2
+assert calculate_fuel(1969) == 654
+assert calculate_fuel(100756) == 33583
 assert total_fuel(os.path.join(currentdir, "testinput.txt"), False) == 34241
 assert total_fuel(os.path.join(currentdir, "testinput.txt"), True) == 51316
+assert calculate_total_fuel(14) == 2
+assert calculate_total_fuel(1969) == 966
+assert calculate_total_fuel(100756) == 50346
 
 print("Part 1: %s" % (total_fuel(os.path.join(currentdir, "input.txt"), False)))
 print("Part 2: %s" % (total_fuel(os.path.join(currentdir, "input.txt"), True)))
--- 01/run_ic01.py
+++ 01/run_ic01.py
@@ -4,7 +4,12 @@ currentdir = os.path.dirname(os.path.abspath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from lib import intcode
+try:
+    from lib.intcode import IntcodeVM
+except ImportError:
+    print("Intcode library could not be found")
+    exit(1)
+
 
 def load_input(filename):
     with open(filename, "r") as file:
@@ -14,23 +19,25 @@ def load_input(filename):
     inputs.append(0)
     return inputs
 
-code1 = intcode.read_intcode(os.path.join(currentdir, "01_part1.ic"))
+
+code1 = IntcodeVM.read_intcode(os.path.join(currentdir, "01_part1.ic"))
+machine = IntcodeVM(code1)
 
 print("Testcases")
-_, outputs = intcode.run_intcode(code1, [12, 0])
+outputs = machine.run([12, 0])
 assert outputs.pop() == 2
-_, outputs = intcode.run_intcode(code1, [14, 0])
+outputs = machine.run([14, 0])
 assert outputs.pop() == 2
-_, outputs = intcode.run_intcode(code1, [1969, 0])
+outputs = machine.run([1969, 0])
 assert outputs.pop() == 654
-_, outputs = intcode.run_intcode(code1, [100756, 0])
+outputs = machine.run([100756, 0])
 assert outputs.pop() == 33583
 
 
 inputs = load_input(os.path.join(currentdir, "testinput.txt"))
-_, outputs = intcode.run_intcode(code1, inputs)
+outputs = machine.run(inputs)
 assert outputs.pop() == 34241
 
 print("Part 1")
 inputs = load_input(os.path.join(currentdir, "input.txt"))
-intcode.run_intcode(code1, inputs)
+machine.run(inputs)
